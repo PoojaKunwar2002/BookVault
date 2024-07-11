a5f1ec5c9176781248e9144c8926a072577f4550
Pooja Kunwar <poojakunwar1311@gmail.com>
2024-07-11 17:38:28 +0530

    Add new changes to BookVault componentAdd new changes to BookVault component .git/COMMIT_EDITMSG[+] [unix] (17:38 11/07/2024)          1,39 Top-- INSERT --Add new changes to Nav.css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                  <Link to="./" class="navbar-brand"> Book Management System</Link>
+                  <Link to="./" class="navbar-brand"> Book-Vault</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
